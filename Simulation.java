6f4b110f44a16ee842579103dd16a92d13a3a8ff
AHS CS <ahs-cs@ausdk12.org>
2024-01-31 15:03:47 -0800

    brev

--- Simulation.java
+++ Simulation.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Write a description of class Simulation here.
  * 
@@ -8,6 +8,7 @@
 public class Simulation {
 
     final static boolean DEBUGGING = false;
+    ArrayList<Player> players = new ArrayList<Player>();
     Player p1 = new TitForTat( );
     Player p2 = new Player ( );
     
@@ -16,38 +17,50 @@ public class Simulation {
     }
     
     public Simulation (int gameCount) {
+        players.add(new TitForTat());
+        players.add(new Unforgiving());
+        players.add(new Competitor());
+        players.add(new Player());
+        players.add(new RandomChooser());
 
-        for (int k=0; k<gameCount; k++) {
-            boolean p1cooperated = p1.cooperates ( );
-            boolean p2cooperated = p2.cooperates ( );
-            if (p1cooperated) {
-                if (p2cooperated) {
-                    p1.increaseScore (3);
-                    p2.increaseScore (3);
+        for (int x=0; x<500; x++) {
+            p1 = players.get((int)(Math.random()*players.size()));
+            p2 = players.get((int)(Math.random()*players.size()));
+            for (int k=0; k<gameCount; k++) {
+                boolean p1cooperated = p1.cooperates ( );
+                boolean p2cooperated = p2.cooperates ( );
+                if (p1cooperated) {
+                    if (p2cooperated) {
+                        p1.increaseScore (3);
+                        p2.increaseScore (3);
+                    } else {
+                        p1.increaseScore (0);
+                        p2.increaseScore (5);
+                    }
                 } else {
-                    p1.increaseScore (0);
-                    p2.increaseScore (5);
+                    if (p2cooperated) {
+                        p1.increaseScore (5);
+                        p2.increaseScore (0);
+                    } else {
+                        p1.increaseScore (1) ;
+                        p2.increaseScore (1);
+                    }
                 }
-            } else {
-                if (p2cooperated) {
-                    p1.increaseScore (5);
-                    p2.increaseScore (0);
-                } else {
-                    p1.increaseScore (1) ;
-                    p2.increaseScore (1);
+                if (DEBUGGING) {
+                    System.out.println ("p1 " + p1cooperated + ", p2 " + p2cooperated);
+                    System.out.println ("p1 score = " + p1.score ( ) + ", p2 score = " + p2.score ( ));
                 }
+        
+                p1.remember (p2cooperated);
+                p2.remember (p1cooperated);
             }
-            if (DEBUGGING) {
-                System.out.println ("p1 " + p1cooperated + ", p2 " + p2cooperated);
-                System.out.println ("p1 score = " + p1.score ( ) + ", p2 score = " + p2.score ( ));
-            }
-    
-            p1.remember (p2cooperated);
-            p2.remember (p1cooperated);
         }
-        System.out.println ("Player 1 has " + p1);
-        System.out.println ("Player 2 has " + p2);
+
+        Collections.sort(players);
+        for(Player p: players) {
+            System.out.println(p);
+        }
     }
-    
+
 
 }
